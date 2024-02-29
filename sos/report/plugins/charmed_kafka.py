b1e63542568830f49fa5f886e430798598c846c5
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2024-02-29 17:48:06 +0000

    feat: add time range filtering plugin option

--- sos/report/plugins/charmed_kafka.py
+++ sos/report/plugins/charmed_kafka.py
@@ -1,16 +1,40 @@
+from datetime import datetime, timedelta
 import json
-from sos.report.plugins import Plugin, UbuntuPlugin
+from sos.report.plugins import Plugin, UbuntuPlugin, PluginOpt
+import glob
+import re
 
 PATHS = {
-    "CONF": f"/var/snap/charmed-kafka/current/etc/kafka",
-    "LOGS": f"/var/snap/charmed-kafka/common/var/log/kafka",
+    "CONF": "/var/snap/charmed-kafka/current/etc/kafka",
+    "LOGS": "/var/snap/charmed-kafka/common/var/log/kafka",
 }
+
+DATE_FORMAT = "%Y-%m-%d-%H"
 
 
 class CharmedKafka(Plugin, UbuntuPlugin):
 
     short_desc = "Charmed Kafka"
     plugin_name = "charmed_kafka"
+
+    current_date = datetime.now()
+    default_date_from = "1970-01-01-00"
+    default_date_to = (current_date + timedelta(hours=1)).strftime(DATE_FORMAT)
+
+    option_list = [
+        PluginOpt(
+            "date-from",
+            default="1970-01-01-00",
+            desc="date from which to filter logs, in format YYYY-MM-DD-HH",
+            val_type=str,
+        ),
+        PluginOpt(
+            "date-to",
+            default=default_date_to,
+            desc="date to which to filter logs, in format YYYY-MM-DD-HH",
+            val_type=str,
+        ),
+    ]
 
     @property
     def bootstrap_server(self) -> str | None:
@@ -30,6 +54,36 @@ class CharmedKafka(Plugin, UbuntuPlugin):
         return f"--bootstrap-server {self.bootstrap_server} --command-config {PATHS['CONF']}/client.properties"
 
     def setup(self):
+        # --- FILE EXCLUSIONS ---
+
+        for file in glob.glob(f"{PATHS['LOGS']}/*"):
+            date = re.search(
+                pattern=r"([0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2})", string=file
+            )
+
+            # include files without date, aka current files
+            if not date:
+                continue
+
+            file_dt = datetime.strptime(date.group(1), DATE_FORMAT)
+
+            if file_dt < datetime.strptime(
+                str(self.get_option("date-from")), DATE_FORMAT
+            ) or file_dt > datetime.strptime(
+                str(self.get_option("date-to")), DATE_FORMAT
+            ):
+                # skip files outside given range
+                self.add_forbidden_path(file)
+
+        # hide keys/stores
+        self.add_forbidden_path(
+            [
+                f"{PATHS['CONF']}/*.pem",
+                f"{PATHS['CONF']}/*.p12",
+                f"{PATHS['CONF']}/*.jks",
+            ]
+        )
+
         # --- FILE INCLUSIONS ---
 
         self.add_copy_spec(
@@ -37,16 +91,6 @@ class CharmedKafka(Plugin, UbuntuPlugin):
                 f"{PATHS['CONF']}",
                 f"{PATHS['LOGS']}",
                 "/var/log/juju",
-            ]
-        )
-
-        # --- FILE EXCLUSIONS ---
-
-        self.add_forbidden_path(
-            [
-                f"{PATHS['CONF']}/*.pem",
-                f"{PATHS['CONF']}/*.p12",
-                f"{PATHS['CONF']}/*.jks",
             ]
         )
 
